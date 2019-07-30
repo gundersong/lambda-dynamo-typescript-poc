dd3d4113e704fec8f25923dd5400cb9b16787929
George Gunderson <george.gunderson@gousto.co.uk>
2019-07-30 11:47:14 +0100

    update logger

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,3 +1,23 @@
 import { LambdaLog } from 'lambda-log';
 
-export const logger = new LambdaLog();
+const keyMap: Array<[string, string]> = [
+  ['msg', 'message'],
+  ['_logLevel', 'log_level'],
+];
+
+const renameProp = (
+  oldKey: string,
+  newKey: string,
+  { [oldKey]: old, ...others },
+) => ({ [newKey]: old, ...others });
+
+export const logger = new LambdaLog({
+  replacer: (key, value) => {
+    if (key === '') {
+      // Replace log object keys
+      return keyMap.reduce((acc, [oldKey, newKey]) => renameProp(oldKey, newKey, acc), value);
+    }
+    return value;
+  },
+  tags: [`${process.env.API_ENTITY}_service`],
+});
